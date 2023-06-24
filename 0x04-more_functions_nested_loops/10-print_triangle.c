0b8819bf944b59f3c5f2126dd8e2f095c463a802
KibulonC <kelvinkibulon98@gmail.com>
2023-06-24 01:54:48 -0700

    Nested Func

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_triangle - prints a triangle, followed by a new line.
- * size: is the size of the triangle
+ * @size: is the size of the triangle
  *
  * Return:
  */
