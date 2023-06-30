5ffe3f91f51be5080cdb059db9835c2eb13d82ad
KibulonC <kelvinkibulon98@gmail.com>
2023-06-30 16:44:29 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,36 +17,27 @@ int _atoi(char *s)
 	dig = 0;
 
 	while (s[len] != '\0')
-	{
 		len++;
-	}
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			dig = s[i] - '0';
 			if (d % 2)
-			{
 				dig = -dig;
-			}
 			n = n * 10 + dig;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
-			}
 			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
-	{
 		return (0);
-	}
 	return (n);
 }
-
