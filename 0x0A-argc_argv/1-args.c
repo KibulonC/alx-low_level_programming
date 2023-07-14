afb16a91d34bda2e3e7de6cacc70381a2bc47d1b
KibulonC <kelvinkibulon98@gmail.com>
2023-07-14 14:23:28 -0700

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints argc(number of arguments)
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ *
+ * Return: Always (0) Success.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	argc -= 1;
+	printf("%d\n", argc);
+	return (0);
+}
